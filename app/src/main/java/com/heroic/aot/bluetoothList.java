91fdd4f38941091a1d04e19d2601d90aaa0bc4f3
KarthickHeroic <karthick.veeramani@gmail.com>
2019-08-16 18:59:47 +0530

    Bluetooth List Created

---
+++ app/src/main/java/com/heroic/aot/bluetoothList.java
@@ -0,0 +1,42 @@
+package com.heroic.aot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class bluetoothList extends AppCompatActivity {
+    ListView devicelist;
+
+    private BluetoothAdapter myBluetooth = null;
+    private Set<BluetoothDevice> pairedDevices;
+    public static String EXTRA_ADDRESS = "device_address";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_list);
+
+        devicelist = (ListView) findViewById(R.id.lv);
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+
+        if ( pairedDevices.size() > 0 ) {
+            for ( BluetoothDevice bt : pairedDevices ) {
+                list.add(bt.getName().toString() + "\n" + bt.getAddress().toString());
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        devicelist.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/heroic/aot/device_connection.java
+++ app/src/main/java/com/heroic/aot/device_connection.java
@@ -2,13 +2,50 @@ package com.heroic.aot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class device_connection extends AppCompatActivity {
+
+    Button selectBTPairedDevice;
+
+    private BluetoothAdapter myBluetooth = null;
+    private Set<BluetoothDevice> pairedDevices;
+    public static String EXTRA_ADDRESS = "device_address";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_connection);
+
+        selectBTPairedDevice = (Button) findViewById(R.id.selectBTDevice);
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+
+        if ( myBluetooth==null ) {
+            Toast.makeText(getApplicationContext(), "Bluetooth device not available", Toast.LENGTH_LONG).show();
+            finish();
+        } else if ( !myBluetooth.isEnabled() ) {
+            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBTon, 1);
+        }
+        selectBTPairedDevice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(device_connection.this,  bluetoothList.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
